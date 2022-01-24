48801f051ebb8081d6b483bf75e11e53cd39c0f2
carlon-python <carlson.python@gmail.com>
2022-01-24 19:42:15 +0200

    3
    +

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=173)
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    description = models.CharField(max_length=174, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
